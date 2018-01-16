e745b67efcb884f216d8c16d20179ced277b9435
rickytranmer <rwtranmer@gmail.com>
2018-01-16 15:31:02 -0700

    google maps api test

---
+++ test/googleMaps_spec.js
@@ -0,0 +1,13 @@
+var request = require('request');
+var expect = require('chai').expect;
+
+describe('Maps API Call', function() {
+	it('to contain google.maps object', function(done) {
+		request('https://maps.googleapis.com/maps/api/js?key=AIzaSyDGTI0Bxwcf9tj39wVLt7HPySSENaRwEvE', function(err, apiRes, body) {
+			// console.log(body);
+			expect(body).to.contain('google.maps');
+			done();
+		});
+	});
+});
+
