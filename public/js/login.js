de05068f2014252a82a8d955333e60aa2a550707
rickytranmer <rwtranmer@gmail.com>
2018-01-16 17:58:59 -0700

    enchanced borders and gradients

--- public/js/login.js
+++ public/js/login.js
@@ -2,4 +2,5 @@ $(function() {
 	console.log('loaded');
 	$('#signUpBtn').css('transition-property', 'none');
 	$('#logInBtn').toggleClass('btn-outline-primary').css('letter-spacing', '4px');
+	$('#map').css('border-radius', '40px');
 });
--- public/js/signup.js
+++ public/js/signup.js
@@ -2,4 +2,5 @@ $(function() {
 	console.log('loaded');
 	$('#logInBtn').css('transition-property', 'none');
 	$('#signUpBtn').toggleClass('btn-outline-primary').css('letter-spacing', '4px');
+	$('#map').css('border-radius', '40px');
 });
