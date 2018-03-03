e5cf32e7cb5488ab31129d0f7a82df4a583e6687
rickytranmer <rwtranmer@gmail.com>
2018-03-03 09:33:27 -0700

    better login/signup menu display and layout

--- public/js/login.js
+++ public/js/login.js
@@ -5,5 +5,5 @@ $(function() {
 	// - Grow Log In button text
 	$('#logInBtn').toggleClass('btn-outline-primary').css('letter-spacing', '4px');
 	// - Rounded edges for form background aka the map div
-	$('#map').css('border-radius', '40px');
+	$('#map').css('border-radius', '40px').css('min-height', '90vh');
 });
--- public/js/signup.js
+++ public/js/signup.js
@@ -5,5 +5,5 @@ $(function() {
 	// - Grow Sign Up button text
 	$('#signUpBtn').toggleClass('btn-outline-primary').css('letter-spacing', '4px');
 	// - Rounded edges for form background aka the map div
-	$('#map').css('border-radius', '40px');
+	$('#map').css('border-radius', '40px').css('min-height', '90vh');
 });
